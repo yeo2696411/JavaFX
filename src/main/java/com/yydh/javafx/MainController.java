f3fd805ad3abf3262f939700b2b57b8a165e9e00
yeo2696411 <yeo2696411@gmail.com>
2022-11-24 23:49:41 +0900

    json추가
    library

--- src/main/java/com/yydh/javafx/MainController.java
+++ src/main/java/com/yydh/javafx/MainController.java
@@ -8,7 +8,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -43,6 +46,29 @@ public class MainController implements Initializable {
 
             Stage primaryStage = (Stage) guideBtn.getScene().getWindow();
             primaryStage.setScene(scene);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            String key = "ARHV0dGRBv97jYLvWQed81svMFj5CfrU9ADtwJgG";
+
+            String urlStr = "https://open-api.bser.io";
+
+            URL url = new URL(urlStr);
+
+            BufferedReader bf;
+            String line = "";
+            String result = "";
+
+            bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+            result = bf.readLine();
+
+            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
