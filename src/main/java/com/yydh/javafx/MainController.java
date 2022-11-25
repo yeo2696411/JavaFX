7255693a442d4ee8fcb845ea8181214a499e1d6e
yeo2696411 <yeo2696411@gmail.com>
2022-11-25 13:00:21 +0900

    asdf
    asdf

--- src/main/java/com/yydh/javafx/MainController.java
+++ src/main/java/com/yydh/javafx/MainController.java
@@ -8,14 +8,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -54,22 +57,66 @@ public class MainController implements Initializable {
     public static void main(String[] args) {
 
         try {
-            String key = "ARHV0dGRBv97jYLvWQed81svMFj5CfrU9ADtwJgG";
 
-            String urlStr = "https://open-api.bser.io";
-
-            URL url = new URL(urlStr);
-
-            BufferedReader bf;
-            String line = "";
-            String result = "";
-
-            bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-            result = bf.readLine();
 
             
 
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String url = "myurl"
+        String authorizationKey = "Basic {인증키}";
+        URL urlObject = null;
+        HttpURLConnection con = null;
+        StringBuffer response = new StringBuffer();
+
+        try {
+            urlObject = new URL(url);
+            con = (HttpURLConnection) urlObject.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Authorization", authorizationKey);
+
+            int responseCode = con.getResponseCode();
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader iny = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String output;
+
+            while ((output = iny.readLine()) != null) {
+                response.append(output);
+            }
+            iny.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            JSONObject json = new JSONObject(response.toString());
+            JSONObject dataJsonObject = json.getJSONObject("data");
+            lastPage = Integer.valueOf(dataJsonObject.optString("last_page"));
+            JSONArray jsonArray = dataJsonObject.getJSONArray("docs");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                FCStockNewsItem item = new FCStockNewsItem();
+                item.sTitle_kor = jsonObject.optString("title", "");
+                item.content = jsonObject.optString("content","");
+//                item.sProffer = sProffer.trim();
+//                item.sNews_Seq = sNews_Seq.trim();
+//                item.sDate = sDate.trim();
+//                item.sTime = sTime.trim();
+//                item.sTitle_eng = sTitle_eng.trim();
+
+//                item.sTrans_yn = sTrans_yn.trim();
+
+                itemList.add(item);
+            }
+            adapter.notifyDataSetChanged();
+
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
